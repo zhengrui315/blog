c88f7cd96b31751bc77ec52e495b523922b2b0fb
Rui Zheng <zhengrui315@gmail.com>
2020-07-25 13:19:06 -0500

    added security configuration with one user

---
+++ src/main/java/com/ruizh/blog/security/LoginUserDetails.java
@@ -0,0 +1,55 @@
+package com.ruizh.blog.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+
+public class LoginUserDetails implements UserDetails {
+    private String userName;
+
+    public LoginUserDetails() {
+    }
+
+    public LoginUserDetails(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return "user";
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
